bb514b25b8efedfd8597d9b1b44d4ed6cd2779df
Tony <antony.branquinho@gmail.com>
2026-03-05 18:58:36 -0300

    adicionei wwwroot/fotos

---
+++ modelos/Relatorios.cs
@@ -0,0 +1,22 @@
+﻿namespace Sistema.modelos
+{
+    public class Relatorios
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int Matricula { get; set; }
+        public string Descricao { get; set; }
+
+        public Relatorios()
+        {
+        }
+
+        public Relatorios(int id, int usuarioId, int matricula, string descricao)
+        {
+            Id = id;
+            UsuarioId = usuarioId;
+            Matricula = matricula;
+            Descricao = descricao;
+        }
+    }
+}
---
+++ modelos/Usuario.cs
@@ -0,0 +1,27 @@
+﻿namespace Sistema.modelos
+{
+    public class Usuario
+    {
+        public int id {  get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+
+        public string SenhaHash { get; set; }
+        public bool Ativo { get; set; }
+
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(int id, string nome, string email, string senhaHash, bool ativo)
+        {
+            this.id = id;
+            Nome = nome;
+            Email = email;
+            SenhaHash = senhaHash;
+            Ativo = ativo;
+        }
+    }
+}
