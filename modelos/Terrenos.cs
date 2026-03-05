fa20e76dfc651ad370b6cca5a781fd79134ce215
Tony <antony.branquinho@gmail.com>
2026-03-05 17:06:12 -0300

    primeiro commite

---
+++ modelos/Terrenos.cs
@@ -0,0 +1,23 @@
+﻿namespace Sistema.Modelos
+{
+    public class Terrenos
+    {
+        public long Id {  get; set; }
+        public string Nome { get; set; }
+        public decimal Area { get; set; }
+        public string Proprietaria { get; set; }
+
+
+        public Terrenos()
+        {
+        }
+
+        public Terrenos(long id, string nome, decimal area, string proprietaria)
+        {
+            Id = id;
+            Nome = nome;
+            Area = area;
+            Proprietaria = proprietaria;
+        }
+    }
+}
